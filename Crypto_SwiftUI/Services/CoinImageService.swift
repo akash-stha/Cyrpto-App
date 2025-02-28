f5d141d0ce64d739e895f0349964585594e7dd53
Akash Shrestha <aakashtha49@gmail.com>
2025-02-27 23:49:47 -0500

    Save images to Fiel Manager

--- Crypto_SwiftUI/Services/CoinImageService.swift
+++ Crypto_SwiftUI/Services/CoinImageService.swift
@@ -13,13 +13,27 @@ class CoinImageService {
     @Published var image: UIImage? = nil
     private var imageSubscription: AnyCancellable?
     private let coinModel: CoinModel
+    private let fileManager = LocalFileManager.instance
+    private let folderName = "coin_images"
+    private let imageName: String
     
     init(coin: CoinModel) {
         self.coinModel = coin
+        self.imageName = coin.id
         getCoinImage()
     }
     
     private func getCoinImage() {
+        if let savedImage = fileManager.getImage(imageName: coinModel.id, folderName: folderName) {
+            image = savedImage
+            print("Retrieved image from file manager.")
+        } else {
+            downloadCoinImage()
+            print("Downloading image now.")
+        }
+    }
+    
+    private func downloadCoinImage() {
         guard let url = URL(string: coinModel.image) else { return }
         
         imageSubscription = NetworkingManager.download(url: url)
@@ -27,8 +41,10 @@ class CoinImageService {
                 return UIImage(data: data)
             })
             .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] returnedImage in
-                self?.image = returnedImage
-                self?.imageSubscription?.cancel()
+                guard let self = self, let downloadedImage = returnedImage else { return }
+                self.image = downloadedImage
+                self.imageSubscription?.cancel()
+                self.fileManager.saveImage(image: downloadedImage, imageName: self.imageName, folderName: self.folderName)
             })
     }
 }
